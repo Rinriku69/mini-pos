4c4887ce4038d524f4a916a7ee58264659b22001
Rinriku69 <siripoohkim@gmail.com>
2026-01-03 14:56:58 +0700

    add store route

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Store } from '../store/store';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'store', component: Store },
+    { path: '', redirectTo: 'store', pathMatch: 'full' }];
