23e7ea06e0ed93d2daf404abbdb25f95e7bca976
Rinriku69 <siripoohkim@gmail.com>
2026-01-03 14:58:20 +0700

    create store template/component

---
+++ front-end/src/store/store.ts
@@ -0,0 +1,25 @@
+import { Component, computed, effect, inject, OnInit, Signal } from '@angular/core';
+import { ProductService } from '../services/product-service';
+import { Product } from '../models/product';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-store',
+  imports: [],
+  templateUrl: './store.html',
+  styleUrl: './store.scss',
+})
+export class Store implements OnInit {
+  private productService = inject(ProductService);
+  products = toSignal(this.productService.productState$, { initialValue: [] })
+  constructor() {
+    effect(() => {
+      console.log('Products changed:', this.products());
+    });
+  }
+  ngOnInit(): void {
+
+
+  }
+
+}
