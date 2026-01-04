7c148f7531adbb7ed079899554e2154673bce537
Rinriku69 <siripoohkim@gmail.com>
2026-01-04 11:43:44 +0700

    add route and navigation link

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Store } from '../store/store';
+import { AddProduct } from './add-product/add-product';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'store', pathMatch: 'full' },
     { path: 'store', component: Store },
-    { path: '', redirectTo: 'store', pathMatch: 'full' }];
+    { path: 'add-product', component: AddProduct },];
