27a4413b6777ba0ad4a606de2d5c6e016e815507
Rinriku69 <siripoohkim@gmail.com>
2026-01-04 17:21:18 +0700

    create category api

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProductController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index']);
