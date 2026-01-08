5802b8b8dff74364efebd293531c3cdd9bb5814e
Rinriku69 <siripoohkim@gmail.com>
2026-01-08 14:43:01 +0700

    add product post route and fillable for mass assignment

--- back-end/app/Models/Product.php
+++ back-end/app/Models/Product.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category_id',
+    ];
     function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -5,4 +5,7 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+// บรรทัดนี้บรรทัดเดียว สำหรับ GET, POST, PUT, DELETE 
+// Route::apiResource('products', ProductController::class);
 Route::get('/categories', [CategoryController::class, 'index']);
