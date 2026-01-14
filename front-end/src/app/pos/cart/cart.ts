d3bb18813a561f96960b3a18f75ec4165ea59448
Rinriku69 <siripoohkim@gmail.com>
2026-01-14 15:03:14 +0700

    create total computed signal for cart items

--- front-end/src/app/pos/cart/cart.ts
+++ front-end/src/app/pos/cart/cart.ts
@@ -1,17 +1,25 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
 import { CartService } from '../services/cart-service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { CartCard } from '../cart-card/cart-card';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
-  imports: [CartCard],
+  imports: [CartCard, DecimalPipe],
   templateUrl: './cart.html',
   styleUrl: './cart.scss',
 })
 export class Cart {
   cartService = inject(CartService)
   protected readonly cart = toSignal(this.cartService.cart$);
+  readonly total = computed(() => this.cart()?.order_item.reduce((result, curr) => {
+    return result + (curr.product.price * curr.qty)
+  }, 0))
 
-
+  constructor() {
+    effect(() => {
+      console.log(this.total())
+    })
+  }
 }
