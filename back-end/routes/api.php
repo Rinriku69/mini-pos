6a95dc25a31c515b49a8f82715868616ca7b5094
Rinriku69 <siripoohkim@gmail.com>
2026-01-15 13:20:38 +0700

    create get api route for orders

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
+use App\Models\Order;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProductController::class, 'index']);
@@ -11,3 +12,4 @@ Route::post('/products', [ProductController::class, 'store']);
 // Route::apiResource('products', ProductController::class);
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/orders', [OrderController::class, 'store']);
+Route::get('/orders', [Order::class, 'index']);
