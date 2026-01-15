44eb9bb5ece40c3238438f87614221c95977202f
Rinriku69 <siripoohkim@gmail.com>
2026-01-15 14:02:50 +0700

    add order-card selector to order view

--- front-end/src/app/pos/orders/orders.ts
+++ front-end/src/app/pos/orders/orders.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { OrderService } from '../services/order-service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { OrderCard } from '../order-card/order-card';
 
 @Component({
   selector: 'app-orders',
-  imports: [],
+  imports: [OrderCard],
   templateUrl: './orders.html',
   styleUrl: './orders.scss',
 })
-export class Orders {
+export class Orders implements OnInit {
+  private orderService = inject(OrderService);
+  readonly orders = toSignal(this.orderService.orders$);
 
+  ngOnInit(): void {
+    this.orderService.loadOrders().subscribe()
+  }
 }
