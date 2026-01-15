62e9f40bf23000c62fdfa19e1bd23df35cdf5cfd
Rinriku69 <siripoohkim@gmail.com>
2026-01-15 14:01:42 +0700

    add order route and nav

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Store } from './pos/store/store';
 import { AddProduct } from './pos/add-product/add-product';
 import { Cart } from './pos/cart/cart';
+import { Orders } from './pos/orders/orders';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'store', pathMatch: 'full' },
     { path: 'store', component: Store },
     { path: 'add-product', component: AddProduct },
     { path: 'cart', component: Cart },
+    { path: 'orders', component: Orders },
 ];
