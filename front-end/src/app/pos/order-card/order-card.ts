4246c2e8291f5059540f231aab57c769bc576832
Rinriku69 <siripoohkim@gmail.com>
2026-01-15 14:02:13 +0700

    create order-card

---
+++ front-end/src/app/pos/order-card/order-card.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { Order } from '../models/types';
+
+@Component({
+  selector: 'app-order-card',
+  imports: [],
+  templateUrl: './order-card.html',
+  styleUrl: './order-card.scss',
+})
+export class OrderCard {
+  order = input.required<Order>();
+
+  
+}
