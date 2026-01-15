1cf4868b4b0712c08e226fbe9b98596361682353
Rinriku69 <siripoohkim@gmail.com>
2026-01-15 13:33:04 +0700

    create loadOrder in order-service

--- front-end/src/app/pos/services/order-service.ts
+++ front-end/src/app/pos/services/order-service.ts
@@ -1,10 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
+import { Order } from '../models/types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrderService {
-  
+  private http = inject(HttpClient)
+  private orderApiUrl = 'http://127.0.0.1:8000/api/orders'
+  private orders = new BehaviorSubject([] as Order[]);
+  orders$ = this.orders.asObservable();
 
-  
+  loadOrders() {
+    this.http.get<Order[]>(this.orderApiUrl).pipe(
+      tap(response => this.orders.next(response))
+    )
+  }
+
 }
