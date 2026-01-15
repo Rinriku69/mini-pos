db4e3e4616483c415c2f9bcd79d12011209cecd0
Rinriku69 <siripoohkim@gmail.com>
2026-01-15 13:19:18 +0700

    create orders and order_items resource

--- back-end/app/Http/Controllers/OrderController.php
+++ back-end/app/Http/Controllers/OrderController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\OrderResource;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
+    function index()
+    {
+        $orders = Order::with('orderItems.product')->get();
+
+        return OrderResource::collection($orders);
+    }
+
     function store(Request $request)
     {
         DB::transaction(function () use ($request) {
