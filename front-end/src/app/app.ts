8fc7adb73cde103514ca00d6c8b01bac35d46a1e
Rinriku69 <siripoohkim@gmail.com>
2026-01-16 20:43:17 +0700

    add cart count and show item count in cart on cart icon

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CartService } from './pos/services/cart-service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,21 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('mini-pos');
+  private cartService = inject(CartService)
+  cart = toSignal(this.cartService.cart$)
+  cartCount = computed(() => {
+    const cart = this.cart()
+    if (!cart) {
+      return 0
+    } else {
+      return cart.order_item.length
+    }
+  })
+
+  constructor() {
+    effect(() => {
+      console.log('cart change' + this.cart()?.order_item)
+    })
+  }
 
 }
