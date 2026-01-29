612efa11727488cad751031b4818a6fd8dec01cc
Rinriku69 <siripoohkim@gmail.com>
2026-01-29 20:26:25 +0700

    create product stat card

--- frontend/src/app/pos/components/home-page/home-page.ts
+++ frontend/src/app/pos/components/home-page/home-page.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject, OnInit } from '@angular/core';
 import { RouterLink, } from "@angular/router";
+import { ProductService } from '../../services/product-service';
+import { toSignal } from '@angular/core/rxjs-interop';
+
 
 @Component({
   selector: 'app-home-page',
@@ -8,5 +11,16 @@ import { RouterLink, } from "@angular/router";
   styleUrl: './home-page.scss',
 })
 export class HomePage {
+  private productService = inject(ProductService)
+  private products = toSignal(this.productService.productState$, { initialValue: [] })
+  productStock = computed(() => this.products().length)
+
+  constructor() {
+    this.productService.loadProduct().subscribe()
+    effect(() => {
+      console.log(this.products())
+    })
+  }
+
 
 }
