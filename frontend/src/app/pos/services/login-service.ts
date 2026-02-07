70cd0e3b24a5b30ee1db36f0cb69a04151f5cb4a
Rinriku69 <siripoohkim@gmail.com>
2026-02-07 20:11:16 +0700

    create redirect after sign up

--- frontend/src/app/pos/services/login-service.ts
+++ frontend/src/app/pos/services/login-service.ts
@@ -2,6 +2,9 @@ import { inject, Injectable } from '@angular/core';
 import { RegisterForm } from '../models/types';
 import { FieldTree } from '@angular/forms/signals';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+
 
 @Injectable({
   providedIn: 'root',
@@ -9,11 +12,13 @@ import { HttpClient } from '@angular/common/http';
 export class LoginService {
   private registerUrl = 'http://127.0.0.1:8000/api/register'
   private http = inject(HttpClient);
+  private router = inject(Router)
 
   createUser(registerForm: FieldTree<RegisterForm>) {
     this.http.post(this.registerUrl, registerForm().value()).subscribe({
       next: (response) => {
         console.log('Successfully Screated', response);
+        this.router.navigate(['/main/store'])
 
       },
       error: (error) => {
