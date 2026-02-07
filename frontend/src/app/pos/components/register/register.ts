53dac405b15a631c9ff71b11de81befece5873f2
Rinriku69 <siripoohkim@gmail.com>
2026-02-07 14:15:07 +0700

    remove reactive form

--- frontend/src/app/pos/components/register/register.ts
+++ frontend/src/app/pos/components/register/register.ts
@@ -1,18 +1,16 @@
-import { Component, effect, inject, Signal, signal, WritableSignal } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { form, minLength ,FormField} from '@angular/forms/signals';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
 import { RegisterForm } from '../../models/types';
 
 
 @Component({
   selector: 'app-register',
-  imports: [formField],
+  imports: [FormField,],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
-  private fb = inject(FormBuilder)
+
   protected registerModel: WritableSignal<RegisterForm> = signal({
     name: '',
     email: '',
@@ -22,17 +20,10 @@ export class Register {
 
   readonly registerForm = form(this.registerModel);
 
-  /* protected registerForm = this.fb.group({
-    name: ['', [Validators.required, Validators.minLength(5)]],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(8)]],
 
-  }) */
 
 
   constructor() {
-    /* effect(() => {
-
-    }) */
+ 
   }
 }
