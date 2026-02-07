dacfa75bba332f9bc7c399400ca1049c406975a1
Rinriku69 <siripoohkim@gmail.com>
2026-02-07 17:36:32 +0700

    make register form validation

--- frontend/src/app/pos/components/register/register.ts
+++ frontend/src/app/pos/components/register/register.ts
@@ -1,15 +1,18 @@
-import { Component, signal, WritableSignal } from '@angular/core';
-import { form, FormField } from '@angular/forms/signals';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { debounce, email, form, FormField, required } from '@angular/forms/signals';
 import { RegisterForm } from '../../models/types';
+import { LoginService } from '../../services/login-service';
+import { JsonPipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-register',
-  imports: [FormField,],
+  imports: [FormField, JsonPipe],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
+  private loginService = inject(LoginService)
 
   protected registerModel = signal<RegisterForm>({
     name: '',
@@ -18,12 +21,18 @@ export class Register {
 
   });
 
-  readonly registerForm = form(this.registerModel);
-  
+  protected readonly registerForm = form(this.registerModel, (path) => {
+    debounce(path.email, 500)
+    required(path.email, { message: 'Email is required' });
+    email(path.email, { message: 'Please enter a valid email address' })
+    required(path.password, { message: 'Password is required' });
+    required(path.name, { message: 'User name is required' });
+    
+  });
+  constructor() { }
 
-
-
-  constructor() {
-
+  registerSubmit(): void {
+    console.log("Submitted")
+    this.loginService.createUser(this.registerForm)
   }
 }
