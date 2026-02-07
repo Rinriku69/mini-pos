516dcaab1aaf0999a16a7dd1ecf8db591d311c0b
Rinriku69 <siripoohkim@gmail.com>
2026-02-07 14:08:17 +0700

    remove reactive form

--- frontend/src/app/pos/components/register/register.ts
+++ frontend/src/app/pos/components/register/register.ts
@@ -1,28 +1,38 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, effect, inject, Signal, signal, WritableSignal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { minLength } from '@angular/forms/signals';
+import { form, minLength ,FormField} from '@angular/forms/signals';
+import { RegisterForm } from '../../models/types';
 
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule],
+  imports: [formField],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
   private fb = inject(FormBuilder)
-  protected registerForm = this.fb.group({
+  protected registerModel: WritableSignal<RegisterForm> = signal({
+    name: '',
+    email: '',
+    password: ''
+
+  });
+
+  readonly registerForm = form(this.registerModel);
+
+  /* protected registerForm = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(5)]],
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(8)]],
 
-  })
-  // private formValue = toSignal(this.registerForm.valueChanges)
+  }) */
+
 
   constructor() {
-    effect(() => {
-      // console.log(this.formValue())
-    })
+    /* effect(() => {
+
+    }) */
   }
 }
