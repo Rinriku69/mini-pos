5280863eb44c690846a929df7f959600320abb52
Rinriku69 <siripoohkim@gmail.com>
2026-02-08 19:18:28 +0700

    create login signal form and validate

--- frontend/src/app/pos/components/login/login.ts
+++ frontend/src/app/pos/components/login/login.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { LoginForm } from '../../models/types';
+import { form, FormField, required } from '@angular/forms/signals';
+import { LoginService } from '../../services/login-service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormField],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  private readonly loginService = inject(LoginService)
+  protected readonly formDisabled = computed(() => {
+    if (this.loginForm().invalid()) {
+      return true
+    }
+    return false
+  })
+  protected loginModel = signal<LoginForm>({
+    name: '',
+    password: ''
+  })
 
+  loginForm = form(this.loginModel, (path) => {
+    required(path.name, { message: 'User name is required' })
+    required(path.password, { message: 'Password is required' })
+  })
+
+  loginSubmit(): void {
+    this.loginService.loginAutherize(this.loginForm);
+  }
 }
