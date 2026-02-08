380c2d1a7bec0779950a3f48195045ff5bb14e32
Rinriku69 <siripoohkim@gmail.com>
2026-02-08 20:37:00 +0700

    create login function

--- frontend/src/app/pos/services/login-service.ts
+++ frontend/src/app/pos/services/login-service.ts
@@ -1,8 +1,9 @@
-import { inject, Injectable } from '@angular/core';
-import { RegisterForm } from '../models/types';
+import { inject, Injectable, signal } from '@angular/core';
+import { LoginForm, LoginResponse, RegisterForm } from '../models/types';
 import { FieldTree } from '@angular/forms/signals';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { TokenStorage } from './token.storage';
 
 
 
@@ -10,27 +11,61 @@ import { Router } from '@angular/router';
   providedIn: 'root',
 })
 export class LoginService {
-  private registerUrl = 'http://127.0.0.1:8000/api/register'
+  private registerUrl = 'http://127.0.0.1:8000/api/auth/register'
+  private loginUrl = 'http://127.0.0.1:8000/api/auth/login'
   private http = inject(HttpClient);
-  private router = inject(Router)
+  private router = inject(Router);
+  private tokenStorage = inject(TokenStorage);
+  registerErrorMessage = signal({
+    name: '',
+    email: '',
+    other: ''
+  });
+  loginErrorMessage = signal('');
 
-  createUser(registerForm: FieldTree<RegisterForm>) {
+  createUser(registerForm: FieldTree<RegisterForm>): void {
     this.http.post(this.registerUrl, registerForm().value()).subscribe({
       next: (response) => {
         console.log('Successfully Screated', response);
-        this.router.navigate(['/main/store'])
+        this.router.navigate(['/main/login'])
 
       },
       error: (error: HttpErrorResponse) => {
+        this.registerErrorMessage.set({
+          name: '',
+          email: '',
+          other: ''
+        })
         if (error.status == 422) {
-          const validationErrors = error.error;
-          alert(validationErrors.message)
+          const validationErrors = error.error.errors;
+          if (validationErrors.name) {
+            this.registerErrorMessage.update((old) => ({ ...old, name: validationErrors.name[0] }));
+          } else if (validationErrors.email) {
+            this.registerErrorMessage.update((old) => ({ ...old, email: validationErrors.email[0] }));
+          } else {
+            this.registerErrorMessage.update((old) => ({ ...old, other: error.error.message }));
+          }
         } else if (error.status == 500) {
           alert('Server run into a problem please try again later')
         } else {
           alert("An error occurred")
         }
 
+      }
+    })
+  }
+
+  loginAutherize(loginForm: FieldTree<LoginForm>): void {
+    this.http.post<LoginResponse>(this.loginUrl, loginForm().value()).subscribe({
+      next: (response) => {
+        this.tokenStorage.set(response.access_token)
+        this.router.navigate(['/main/store'])
+        console.log(response)
+      },
+
+      error: (err: HttpErrorResponse) => {
+        console.log(err)
+        this.loginErrorMessage.set(err.error.error)
       }
     })
   }
