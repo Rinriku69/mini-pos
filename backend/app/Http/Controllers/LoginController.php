07c728436db9b0a6851c7a8697a0da1ae894d1d7
Rinriku69 <siripoohkim@gmail.com>
2026-02-08 11:52:23 +0700

    create validation for register before create user

--- backend/app/Http/Controllers/LoginController.php
+++ backend/app/Http/Controllers/LoginController.php
@@ -5,21 +5,34 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
     function register(Request $request)
     {
-        DB::transaction(function () use ($request) {
+        $validated = $request->validate([
+            'name' => ['required' . 'string', 'max:20'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'string', 'confirmed']
+        ]);
 
-            $user = new User();
-            $user->email = $request['email'];
-            $user->name = $request['name'];
-            $user->password = $request['password'];
-            $user->role = "USER";
-            $user->save();
-        });
+        $user = new User();
+        $user->email = $request['email'];
+        $user->name = $request['name'];
+        $user->password = $request['password'];
+        $user->role = "USER";
+        $user->save();
 
-        return response()->json(['status' => 'ok']);
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'User created Successfully',
+            'data' => $user->name
+        ], 201);
     }
+
+    /* function authenticate(Request $request){
+        $valida
+    } */
 }
