8cd3077945e2844bf2ace9946cd6b53ecb51b464
Rinriku69 <siripoohkim@gmail.com>
2026-02-18 20:48:36 +0700

    Implement new http request method httpResource instead of httpClient on GET api

--- frontend/src/app/pos/components/store/store.ts
+++ frontend/src/app/pos/components/store/store.ts
@@ -1,10 +1,12 @@
-import { Component, computed, effect, inject, OnInit, Signal } from '@angular/core';
+import { Component, computed, effect, inject, OnInit, signal, Signal } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/types';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ProductCard } from "./product-card/product-card";
 import { CartService } from '../../services/cart.service';
 import { NavService } from '../../services/nav.service';
+import { loadProduct } from '../../helpers';
+
 
 @Component({
   selector: 'app-store',
@@ -12,35 +14,26 @@ import { NavService } from '../../services/nav.service';
   templateUrl: './store.html',
   styleUrl: './store.scss',
 })
-export class Store implements OnInit {
+export class Store {
   private productService = inject(ProductService);
   private cartService = inject(CartService);
   private navService = inject(NavService);
-  private products = toSignal(this.productService.productState$, { initialValue: [] })
-  private searchKey = toSignal(this.navService.searchKey$)
-  protected readonly productsDisplay: Signal<Product[] | null> = computed(() => {
 
-    const products = this.products()
-    const searchKey = this.searchKey()
-    if (!products || !searchKey) return this.products()
+  private products = loadProduct();
+  protected readonly productsDisplay = computed<Product[] | null>(() => {
+    if (!this.products.hasValue()) return null;
 
+    const products = this.products.value().data;
+    const searchKey = this.searchKey();
+    if (!searchKey) return this.products.value().data;
     return products.filter((p) => p.product_name.toLowerCase().includes(searchKey.toLowerCase()) ?
       p.product_name.toLowerCase().includes(searchKey.toLowerCase()) : p.product_description.toLowerCase().includes(searchKey.toLowerCase())
     )
-
   })
+
+  private searchKey = toSignal(this.navService.searchKey$)
   cart = toSignal(this.cartService.cart$)
+  
   constructor() { }
-  ngOnInit(): void {
-    if (this.products().length == 0) {
-      this.getProduct()
-
-    }
-
-  }
-
-  getProduct(): void {
-    this.productService.loadProduct().subscribe();
-  }
 
 }
