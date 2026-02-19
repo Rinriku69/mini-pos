be4de9c75253c8ed86f64a45e83cea6026cefdae
Rinriku69 <siripoohkim@gmail.com>
2026-02-19 18:48:28 +0700

    feat: Product delete functionality

--- frontend/src/app/pos/components/stocks/product-stock/product-stock-card/product-stock-card.ts
+++ frontend/src/app/pos/components/stocks/product-stock/product-stock-card/product-stock-card.ts
@@ -1,7 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Product } from '../../../../models/types';
 import { DecimalPipe } from '@angular/common';
 import { Icon } from "../../../../pages/icons/icon/icon";
+import { ProductService } from '../../../../services/product.service';
 
 @Component({
   selector: 'app-product-stock-card',
@@ -11,4 +12,10 @@ import { Icon } from "../../../../pages/icons/icon/icon";
 })
 export class ProductStockCard {
   readonly product = input.required<Product>();
+  productService = inject(ProductService);
+
+  delete(id: number) {
+
+    this.productService.deleteProduct(id);
+  }
 }
