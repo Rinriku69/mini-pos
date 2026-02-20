35ca6959944b58d95d8ae459a33cc1a9d51a0529
Rinriku69 <siripoohkim@gmail.com>
2026-02-20 20:40:51 +0700

    feat: Edit Form View

--- frontend/src/app/pos/components/stocks/product-stock/product-stock-card/product-stock-card.ts
+++ frontend/src/app/pos/components/stocks/product-stock/product-stock-card/product-stock-card.ts
@@ -1,21 +1,28 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, effect, inject, input, output, signal } from '@angular/core';
 import { Product } from '../../../../models/types';
 import { DecimalPipe } from '@angular/common';
 import { Icon } from "../../../../pages/icons/icon/icon";
 import { ProductService } from '../../../../services/product.service';
+import { EditForm } from '../../edit-form/edit-form';
 
 @Component({
   selector: 'app-product-stock-card',
-  imports: [DecimalPipe, Icon],
+  imports: [Icon, EditForm],
   templateUrl: './product-stock-card.html',
   styleUrl: './product-stock-card.scss',
 })
 export class ProductStockCard {
   readonly product = input.required<Product>();
   productService = inject(ProductService);
+  openEdit = signal(false);
+
+  toggleEdit() {
+    this.openEdit.set(!this.openEdit())
+  }
 
   delete(id: number) {
 
     this.productService.deleteProduct(id);
   }
+
 }
