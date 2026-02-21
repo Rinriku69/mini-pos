4203a60164b3b6d2c87c7ea6cf28f6b9abedbd5a
Rinriku69 <siripoohkim@gmail.com>
2026-02-21 12:03:40 +0700

    merge: resolve conflicts with feature/stock-management

--- frontend/src/app/pos/auth.interceptor.ts
+++ frontend/src/app/pos/auth.interceptor.ts
@@ -1,6 +1,7 @@
-import { inject } from "@angular/core";
-import { TokenStorage } from "./services/token.storage";
 import { HttpInterceptorFn } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { AuthService } from "./services/auth.service";
+import { catchError, throwError } from "rxjs";
 
 
 
@@ -8,15 +9,27 @@ import { HttpInterceptorFn } from "@angular/common/http";
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const token = localStorage.getItem('ng-token') ?? null
-
+    const authService = inject(AuthService);
+    let setHeder = req;
     if (token) {
         const setHeader = req.clone({
             setHeaders: {
                 Authorization: `Bearer ${token}`
             }
         })
-        return next(setHeader)
+
     }
-    return next(req)
+    return next(setHeder).pipe(
+        catchError((err) => {
+            if (err.status === 401) {
+                console.warn('Token Expired')
+
+                authService.clearLocalSession()
+            }
+
+            return throwError(() => err)
+
+        })
+    )
 
 }
